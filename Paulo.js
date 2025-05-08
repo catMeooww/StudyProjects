86d3fb0a2d71aaf40ae61eea496041e9a2fa1246
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2025-05-08 17:08:37 -0300

    Paulo Leminski

--- Paulo.js
+++ Paulo.js
@@ -22,7 +22,19 @@ Poemas = [
     {
         "title":"Por um lindésimo de segundo",
         "body":"tudo em mim<br>anda a mil<br>tudo assim<br>tudo por um fio<br>tudo estivesse no cio<br>tudo pisando macio<br>tudo psiu<br><br>tudo em minha volta<br>anda às tontas<br>como se todas as coisas<br>fossem todas<br>afinal das contas"
-    }
+    },
+    {
+        "title":"Apagar-me",
+        "body":"Apagar-me<br>diluir-me<br>desmanchar-me<br>até que depois de mim<br>de nós<br>de tudo<br>não reste mais<br>que o charme."
+    },
+    {
+        "title":" ÓPERA FANTASMA",
+        "body":"Nada tenho.Nada me pode ser tirado.<br>Eu sou o ex-estranho,<br>o que veio sem ser chamado<br>e, gato se foi<br>sem fazer nenhum ruído"
+    },
+    {
+        "title":"Além Alma<br>(uma grama depois)",
+        "body":"Meu coração lá longe<br>faz sinal que quer voltar<br>Já no peito trago em bronze<br>NÃO HÁ VAGA NEM LUGAR<br>Pra que me serve esse negócio<br>que não cessa de bater?<br>Mais parece um relógio<br>que acabar de enlouquecer<br>Pra que é que eu quero quem chora,<br>se estou tão bm assim,<br>e o vazio que vai lá lá fora<br>cai macio dentro de mim?"
+    },
 ]
 
 function generatePoem(){
@@ -42,55 +54,91 @@ function redirectPoem(){
 
 generatePoem();
 
+document.getElementById("MENU-Options").style.visibility = "hidden";
+function toggleMenu(){
+    menu = document.getElementById("MENU-Options")
+    if (menu.style.visibility == "hidden"){
+        menu.style.visibility = "visible";
+        menu.style.height = String(window.innerHeight) + "px";
+        document.getElementById("Paulo-MENU").style.backgroundColor = "rgba(0, 0, 0, 0.600)";
+        document.getElementById("Paulo-MENU").style.width = String(window.innerWidth) + "px";
+    }else{
+        menu.style.visibility = "hidden";
+        menu.style.height = "0px";
+        document.getElementById("Paulo-MENU").style.backgroundColor = "rgba(0, 0, 0, 0.100)";
+        document.getElementById("Paulo-MENU").style.width = "50px";
+    }
+}
+
 function wikihover(){
-    document.getElementById("wiki").style.marginBottom = "0px";
+    //document.getElementById("wiki").style.marginBottom = "0px";
     document.getElementById("wikiframe").style.width = "420px";
     document.getElementById("wikiframe").style.height = "300px";
+    document.getElementById("wiki").style.height = "304px";
     document.getElementById("wikitext").style.width = "200px";
-    document.getElementById("wikitext").style.height = "300px";
+    document.getElementById("wikitext").style.height = "304px";
     document.getElementById("wikitext").style.visibility = "visible";
 }
 function wikileave(){
-    document.getElementById("wiki").style.marginBottom = "400px";
+    //document.getElementById("wiki").style.marginBottom = "400px";
     document.getElementById("wikiframe").style.width = "250px";
     document.getElementById("wikiframe").style.height = "150px";
+    document.getElementById("wiki").style.height = "154px";
     document.getElementById("wikitext").style.width = "0px";
     document.getElementById("wikitext").style.height = "0px";
     document.getElementById("wikitext").style.visibility = "hidden";
 }
 
 function frasehover(){
-    document.getElementById("frase").style.marginBottom = "0px";
+    //document.getElementById("frase").style.marginBottom = "0px";
     document.getElementById("fraseframe").style.width = "420px";
     document.getElementById("fraseframe").style.height = "300px";
+    document.getElementById("frase").style.height = "304px";
     document.getElementById("frasetext").style.width = "200px";
-    document.getElementById("frasetext").style.height = "300px";
+    document.getElementById("frasetext").style.height = "304px";
     document.getElementById("frasetext").style.visibility = "visible";
 }
 function fraseleave(){
-    document.getElementById("wiki").style.marginBottom = "400px";
+    //document.getElementById("wiki").style.marginBottom = "400px";
     document.getElementById("fraseframe").style.width = "250px";
     document.getElementById("fraseframe").style.height = "150px";
+    document.getElementById("frase").style.height = "154px";
     document.getElementById("frasetext").style.width = "0px";
     document.getElementById("frasetext").style.height = "0px";
     document.getElementById("frasetext").style.visibility = "hidden";
 }
 
 function livroshover(){
-    document.getElementById("livro").style.marginBottom = "0px";
+    //document.getElementById("livro").style.marginBottom = "0px";
     document.getElementById("livrosframe").style.width = "420px";
     document.getElementById("livrosframe").style.height = "300px";
+    document.getElementById("livro").style.height = "304px";
     document.getElementById("livrostext").style.width = "200px";
-    document.getElementById("livrostext").style.height = "300px";
+    document.getElementById("livrostext").style.height = "304px";
     document.getElementById("livrostext").style.visibility = "visible";
 }
 function livrosleave(){
-    document.getElementById("livro").style.marginBottom = "400px";
+    //document.getElementById("livro").style.marginBottom = "400px";
     document.getElementById("livrosframe").style.width = "250px";
     document.getElementById("livrosframe").style.height = "150px";
+    document.getElementById("livro").style.height = "154px";
     document.getElementById("livrostext").style.width = "0px";
     document.getElementById("livrostext").style.height = "0px";
     document.getElementById("livrostext").style.visibility = "hidden";
+}
+
+function redirect(n){
+    if (n == 0){
+        location = "#Paulo-Biografia";
+    }else if (n == 1){
+        location = "#Paulo-Poemas";
+        generatePoem()
+    }else if (n == 2){
+        location = "#Paulo-Campanha";
+    }else if (n == 3){
+        location = "#Paulo-Links";
+    }
+    toggleMenu();
 }
 
 var testMobile = /iPhone|Android|iPad/i.test(navigator.userAgent);
