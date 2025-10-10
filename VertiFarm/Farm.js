5350b39a80bdb5d7da7936a3dec2655ea56bb8eb
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2025-10-10 18:58:03 -0300

    VertiFarm
    Added 3D Model and Responsivity

--- VertiFarm/Farm.js
+++ VertiFarm/Farm.js
@@ -1,24 +1,34 @@
-function loadBody(){
+function loadBody(b) {
     var testMobile = /iPhone|Android|iPad/i.test(navigator.userAgent);
-    if (testMobile){
-        document.getElementById("project-showcaser").style.display = "block";
-        document.getElementById("farmview").style.width = "100%";
-        document.getElementById("farmview").style.height = "200px";
-        document.getElementById("farmdescript").style.width = "100%";
-        document.getElementById("farmdescript").style.height = "300px";
+    if (testMobile) {
         document.getElementById("menu").innerHTML = "<button onclick='mobileMenu()'>Mais</button>";
+        document.getElementById("body").style.marginTop = "18%";
+        if (b == "index") {
+            document.getElementById("project-showcaser").style.display = "block";
+            document.getElementById("farmview").style.width = "100%";
+            document.getElementById("farmview").style.height = "250px";
+            document.getElementById("farmdescript").style.width = "100%";
+            document.getElementById("farmdescript").style.height = "300px";
+
+            for(frame of document.getElementsByTagName("iframe")){
+                frame.style.width = "100%";
+            }
+            for(frame of document.getElementsByClassName("table")){
+                frame.style.width = "100%";
+            }
+        }
     }
 }
 
 slide = 0;
 slides = {
-    "images":[
+    "images": [
         "slide_1.png",
         "slide_2.png",
         "slide_3.png",
         "slide_4.png"
     ],
-    "text":[
+    "text": [
         "<p>Imagem representativa do projeto gerada por IA</p><p>Mostra o conceito da pequena fazenda vertical controlada por computador.</p>",
         "<p>Estrutura Base</p><p>Mostra estrutura com a terra e as plantas, mostra também as <i>Glow Lights</i>, responsáveis por simular iluminação solar para as plantas.</p>",
         "<p>Sistema Interno</p><p>Mostra detalhes da prateleira, como reservatório de agua, sistema de irrigação e sensores como umidade do solo/ar, temperatura, iluminação, ph, etc...</p><p>Esta parte do sistema é vital para que a IA consiga verificar o estado das plantas e fazer ajustes nescessários para manter-las vivas.</p>",
@@ -26,8 +36,8 @@ slides = {
     ]
 }
 
-function updateSlide(){
-    image = "../icons/"+slides["images"][slide];
+function updateSlide() {
+    image = "../icons/" + slides["images"][slide];
     text = slides["text"][slide];
     document.getElementById("viewimage").src = image;
     document.getElementById("farmdescript").innerHTML = text;
@@ -39,28 +49,28 @@ function updateSlide(){
     document.getElementById("viewimage").style.borderColor = "rgb(" + colorR + "," + colorG + "," + colorB + ")";
 }
 
-function nextslide(){
+function nextslide() {
     slide += 1;
-    if (slide > 3){
+    if (slide > 3) {
         slide = 0;
     }
     updateSlide();
 }
 
-function backslide(){
+function backslide() {
     slide -= 1;
-    if (slide < 0){
+    if (slide < 0) {
         slide = 3;
     }
     updateSlide();
 }
 
-function mobileMenu(){
+function mobileMenu() {
     document.getElementById("menu").style.height = "100%";
-    document.getElementById("menu").innerHTML = '<button onclick="closeMenu()">Fechar</button><br><br><br><a href="https://docs.google.com/document/d/1jCiAykNMrb5-mVNDbZW3jvAXNWw17NgQSs3xEMppY_Y/edit?usp=sharing">Pré-Projeto</a><br><br><br><a href="">Artigo</a><br><br><br>';
+    document.getElementById("menu").innerHTML = '<button onclick="closeMenu()">Fechar</button><br><br><br><a href="./">Página Inicial</a><br><br><br><a href="https://docs.google.com/document/d/1jCiAykNMrb5-mVNDbZW3jvAXNWw17NgQSs3xEMppY_Y/edit?usp=sharing">Pré-Projeto</a><br><br><br><a href="">Artigo</a><br><br><br><a href="./model.html">Model</a><br><br><br>';
 }
 
-function closeMenu(){
+function closeMenu() {
     document.getElementById("menu").style.height = "auto";
     document.getElementById("menu").innerHTML = "<button onclick='mobileMenu()'>Mais</button>";
 }
